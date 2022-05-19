97717da51be4f0bf6c0da0dff088e8a5d1541b02
qozymandias <odowning19@gmail.com>
2022-05-20 00:33:06 +1000

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,20 @@
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
-tar xzf nvim-macos.tar.gz
-# sudo -/nvim-osx64/bin/nvim#!/bin/sh
+#!/bin/bash
 
+NVIM_NAME=""
+NVIM_URL=""
+
+if [ "$(uname)" == "Darwin" ]; then
+    NVIM_NAME="nvim-macos.tar.gz"
+    NVIM_URL="https://github.com/neovim/neovim/releases/download/nightly/"$NVIM_NAME
+    curl -LO $NVIM_URL
+    tar xzf nvim-macos.tar.gz
+    sudo rsync -a nvim-osx64/* /usr/local/
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    NVIM_NAME="nvim.appimage"
+    NVIM_URL="https://github.com/neovim/neovim/releases/download/nightly/"$NVIM_NAME
+    curl -LO $NVIM_URL
+    chmod u+x ./nvim.appimage
+    mv ./nvim.appimage /usr/local/bin/nvim
+fi
+
+nvim +'PlugInstall'
