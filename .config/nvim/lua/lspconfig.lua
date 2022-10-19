d4c9290ad43de7154a63916420d9d738b23f8587
Oscar Downing <oscar.downing@dolby.com>
2022-10-19 11:07:00 +1100

    update

--- .config/nvim/lua/lspconfig.lua
+++ .config/nvim/lua/lspconfig.lua
@@ -114,7 +114,7 @@ vim.diagnostic.config({
 
 local lsp_path = "~/.local/share/nvim/lsp_servers/"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 local flag_args = { debounce_text_changes = 150 }
 
 local lspconfig = require 'lspconfig'
