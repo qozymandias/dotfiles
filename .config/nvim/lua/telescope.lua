b084dae9a3a91b46035513c5c5237bb871fb41cb
odown <oscar.downing@dolby.com>
2023-04-18 12:13:13 +1000

    update

--- .config/nvim/lua/telescope.lua
+++ .config/nvim/lua/telescope.lua
@@ -42,8 +42,8 @@ require 'telescope'.setup {
         layout_config = {
             horizontal = {
                 prompt_position = "top",
-                preview_width = 0.55,
-                results_width = 0.8,
+                preview_width = 0.8,
+                results_width = 0.2,
             },
             vertical = {
                 mirror = false,
