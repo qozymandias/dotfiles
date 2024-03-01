3863806ee42cbbc31f98a278c0abce4ee821e418
qozymandias <odowning19@gmail.com>
2024-03-01 14:29:27 +1100

    minor fix

--- .config/nvim/lua/treesitter-context.lua
+++ .config/nvim/lua/treesitter-context.lua
@@ -1,19 +1,12 @@
-require 'treesitter-context'.setup {
+require'treesitter-context'.setup{
     enable = true,
     max_lines = 0,
-    patterns = {
-        default = {
-            'class',
-            'function',
-            'method',
-            'for',
-            'while',
-            'if',
-            'switch',
-            'case',
-        },
-    },
-    exact_patterns = {
-    },
+    min_window_height = 0,
+    line_numbers = true,
+    multiline_threshold = 20,
+    trim_scope = 'outer',
+    mode = 'cursor',
+    separator = nil,
     zindex = 20,
+    on_attach = nil,
 }
